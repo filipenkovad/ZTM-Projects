7038845815c0209a6471e00447cc4cd9b7d4141d
filipenkovad <filipenko.vad@example.com>
2020-07-03 18:10:25 +0300

    Working server with save data to CSV

--- FlaskServer/server.py
+++ FlaskServer/server.py
@@ -1,30 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import csv
 app = Flask(__name__)
 
-@app.route('/')
-def my_page():
-	return render_template('index.html') 
+@app.route('/<string:page_name>')
+def my_page(page_name):
+	return render_template(page_name)
 
-@app.route('/index.html')
-def my_page0():
-	return render_template('index.html') 
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+	if request.method == 'POST':
+		data = request.form.to_dict()
+		write_to_csv(data)
+		return redirect('thankyou.html')
+	else:
+		return 'something go wrong'
 
-@app.route('/about.html')
-def my_page1():
-	return render_template('about.html') 
-
-@app.route('/contact.html')
-def my_page2():
-	return render_template('contact.html') 
-
-@app.route('/work.html')
-def my_page3():
-	return render_template('work.html') 
-
-@app.route('/works.html')
-def my_page4():
-	return render_template('works.html') 
-
-@app.route('/components.html')
-def my_page5():
-	return render_template('components.html') 
+def write_to_csv(data):
+	with open('database.csv', newline='', mode='a') as database:
+		email = data['Email']
+		subject = data['Subject']
+		message = data['Message']
+		csv_writer = csv.writer(database, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+		csv_writer.writerow([email,subject,message])
+		print(email,subject,message)
